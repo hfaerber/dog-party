e3cf7792e448db8736601d66bd3e0772fe305162
Heather Faerber <heatherdfaerber@gmail.com>
2019-08-17 18:42:20 -0600

    Correct style guide items missed the first time

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-var textBoxVar = document.querySelector(".js-text-box");
-var clickButton = document.querySelector(".js-name-button");
-var textToChange = document.querySelector(".js-site-about-blank");
+var textBoxVar = document.querySelector('.js-text-box');
+var clickButton = document.querySelector('.js-name-button');
+var textToChange = document.querySelector('.js-site-about-blank');
 
-clickButton.addEventListener("click", function () {
+clickButton.addEventListener('click', function() {
   var updateTextValue = textBoxVar.value;
   textToChange.innerText = `${updateTextValue}`;
-  textBoxVar.value = "";
+  textBoxVar.value = '';
 });
